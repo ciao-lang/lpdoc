edbca414f41ef447d2549df8a29a778755537cd6
Manuel Hermenegildo <manuel.hermenegildo@imdea.org>
2020-03-25 12:51:29 +0100

    (lpdoc) Minor changes to comment in lpdoc SETTINGS_DEFAULT.pl.
    Src-commit: dc5d7844ffa972e7850701b3c172b4dc888e7044

--- etc/SETTINGS_DEFAULT.pl
+++ etc/SETTINGS_DEFAULT.pl
@@ -1,15 +1,17 @@
 :- module(_, [], [doccfg]).
 
-%! \title  Default configuration file for LPdoc
+%! \title  Default configuration file for running LPdoc
 %  \author The Ciao Development Team
 %
-%  \module This is a default configuration file for \apl{lpdoc},
-%    typically used in the generation of documentation for single
-%    modules. The defaults listed are typically suggestions. These
-%    settings should be changed to suit your application.
+%  \module This is a default configuration file for running
+%    \apl{lpdoc}. It is typically used as default configuration when
+%    generating documentation for single modules, if the user has not
+%    provided such a file. The defaults listed are typically
+%    suggestions. These settings should be changed to suit your
+%    application.
 
 :- doc(bug, "Definitions that are overriden by the emacs mode must fit
-   in one line. Do not use emacs but LPdoc to generate this file").
+   in one line. Do not use Emacs but LPdoc to generate this file.").
 
 filepath := '/path/to/doc/sources'|'/other/path/to/doc/sources'.
 
