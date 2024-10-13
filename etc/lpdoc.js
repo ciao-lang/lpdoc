e9a7e22a4dfbdd8fac7c6b1c1964209a4f9cc9b3
Jose F. Morales <jfmcjf@gmail.com>
2024-10-13 16:02:39 -0500

    Bump Ciao version to 1.24
    Src-commit: 8123d52c941a1c2f134e2d6fcc68db13b9c462bb

--- etc/lpdoc.js
+++ etc/lpdoc.js
@@ -461,7 +461,7 @@ function update_github_stars_theme() {
 /* Load playground functionality if needed */
 
 var urlPREFIX = null; /* global */
-var urlVERS = "?v=1";  /* NOTE: 1.23.0 -> 1; increment for each playground version  */
+var urlVERS = "?v=2";  /* NOTE: 1.24.0 -> 2; increment for each playground version  */
 
 /* resolve a URL: add urlPREFIX for different roots, and urlVERS
    version suffix to avoid browser caching */
